209c9700ef469bd092fd4bc6cb4c6d483ed7bdab
Vladimir Sidorov <v.sidorov29091988@gmail.com>
2025-04-05 12:45:16 +0700

    Refactor #2 - Update table

--- database.sql
+++ database.sql
@@ -1,153 +1,150 @@
 CREATE TABLE courses (
-  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  title VARCHAR(255) UNIQUE NOT NULL,
-  description VARCHAR(255) UNIQUE NOT NULL,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP,
+  id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  title VARCHAR(255) NOT NULL,
+  description text,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   is_deleted TINYINT DEFAULT 0,
 );
 CREATE TABLE lessons (
-  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  title VARCHAR(255) UNIQUE NOT NULL,
-  content VARCHAR(255) UNIQUE NOT NULL,
-  url_video VARCHAR(255) UNIQUE NOT NULL,
+  id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  title VARCHAR(255) NOT NULL,
+  content text,
+  url_video VARCHAR(255),
   pozition INT UNIQUE NOT NULL,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP,
-  url_course VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  url_course VARCHAR(255),
   is_deleted TINYINT DEFAULT 0,
-  course_id INT REFERENCES courses (id) UNIQUE NOT NULL
+  course_id BIGINT REFERENCES courses (id) NOT NULL
 );
 
 
 CREATE TABLE modules (
-  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  title VARCHAR(255) UNIQUE NOT NULL,
-  description VARCHAR(255) UNIQUE NOT NULL,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP,
+  id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  title VARCHAR(255) NOT NULL,
+  description text,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   is_deleted TINYINT DEFAULT 0,
 );
 
 CREATE TABLE programs (
-  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  title VARCHAR(255) UNIQUE NOT NULL,
-  price NUMERIC,
+  id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  title VARCHAR(255) NOT NULL,
+  price NUMERIC(10,2),
   type VARCHAR(255),
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 CREATE TABLE course_modules (
-    course_id INT NOT NULL,
-    module_id INT NOT NULL,
-    PRIMARY KEY (course_id, module_id),
-    FOREIGN KEY (course_id) REFERENCES courses(id),
-    FOREIGN KEY (module_id) REFERENCES modules(id)
+    course_id BIGINT REFERENCES courses(id),
+    module_id BIGINT NREFERENCES modules(id),
+    PRIMARY KEY (course_id, module_id)
 );
 
 CREATE TABLE program_modules (
-    program_id INT NOT NULL,
-    module_id INT NOT NULL,
-    PRIMARY KEY (program_id, module_id),
-    FOREIGN KEY (program_id) REFERENCES programs(id),
-    FOREIGN KEY (module_id) REFERENCES modules(id)
+    program_id BIGINT REFERENCES programs(id),
+    module_id BIGINT REFERENCES modules(id),
+    PRIMARY KEY (program_id, module_id)
 );
+
 CREATE TABLE teaching_groups (
-  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   slug VARCHAR(255) NOT NULL,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP,
-  user_id INT REFERENCES users(id) NOT NULL
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TYPE user_role AS ENUM ('Студент', 'Учитель', 'Админ');
 CREATE TABLE users (
-  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  teaching_group_id BIGINT REFERENCES teaching_groups(id),
   first_name VARCHAR(255) NOT NULL,
   email VARCHAR(255) UNIQUE NOT NULL,
   password_hash VARCHAR(255) NOT NULL,
   url_group VARCHAR(255) NOT NULL,
   role user_role,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 
 CREATE TYPE status AS ENUM ('active', 'pending', 'cancelled', 'completed');
 CREATE TABLE enrollments (
-  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  user_id INT REFERENCES users(id) NOT NULL,
-  program_id INT REFERENCES programs(id) NOT NULL,
+  id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  user_id BIGINT REFERENCES users(id) NOT NULL,
+  program_id BIGINT REFERENCES programs(id) NOT NULL,
   status status,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TYPE status_payments AS ENUM ('pending', 'paid', 'failed', 'refunded');
 
 CREATE TABLE payments (
-  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  enrollment_id INT REFERENCES enrollments(id) NOT NULL,
-  amount NUMERIC NOT NULL,
+  id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  enrollment_id BIGINT REFERENCES enrollments(id) NOT NULL,
+  amount NUMERIC(10,2),
   status status_payments,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE program_completions (
-   id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-   user_id INT REFERENCES users(id) NOT NULL,
-   program_id INT REFERENCES programs(id) NOT NULL,
+   id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+   user_id BIGINT REFERENCES users(id) NOT NULL,
+   program_id BIGINT REFERENCES programs(id) NOT NULL,
    status status,
-   begin TIMESTAMP,
-   end TIMESTAMP,
-   created_at TIMESTAMP,
-   updated_at TIMESTAMP
+   begin DATE,
+   end DATE,
+   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE sertificates (
-  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  user_id INT REFERENCES users(id) NOT NULL,
-  program_id INT REFERENCES programs(id) NOT NULL,
-  url VARCHAR(255) NOT NULL,
-  relese TIMESTAMP,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP
+  id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  user_id BIGINT REFERENCES users(id) NOT NULL,
+  program_id BIGINT REFERENCES programs(id) NOT NULL,
+  url VARCHAR(255),
+  relese DATE,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE quizzes (
-  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  lesson_id INT REFERENCES lessons(id) NOT NULL,
+  id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  lesson_id BIGINT REFERENCES lessons(id) NOT NULL,
   title VARCHAR(255) UNIQUE NOT NULL,
   content JSONB,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE exercises (
-  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  lesson_id INT REFERENCES lessons(id) NOT NULL,
+  id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  lesson_id BIGINT REFERENCES lessons(id) NOT NULL,
   title VARCHAR(255) UNIQUE NOT NULL,
   url VARCHAR(255) NOT NULL,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE discussions (
-  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  lesson_id INT REFERENCES lessons(id) NOT NULL,
-  user_id INT REFERENCES users(id) NOT NULL,
+  id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  lesson_id BIGINT REFERENCES lessons(id) NOT NULL,
+  user_id BIGINT REFERENCES users(id) NOT NULL,
   text JSONB,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 CREATE TYPE status_blog AS ENUM ('created', 'in moderation', 'published', 'archived');
 CREATE TABLE blogs (
-  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  user_id INT REFERENCES users(id) NOT NULL,
+  id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  user_id BIGINT REFERENCES users(id) NOT NULL,
   title VARCHAR(255) NOT NULL,
   text text NOT NULL,
   status status_blog,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
